eebd5c0439a26393859809dc511b37b7d45744e2
Anubhav Gupta <anubhavg0098@gmail.com>
2026-06-03 19:08:51 +0530

    Short term history workflow added

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,9 @@ def main():
     
     print("Research Agent Started")
     print("Type 'exit' to quit\n")
+    print("Type clear to reset conversation history\n")
+    
+    conversation_history = []
     
     while True:
         query = input("Ask a question: ")
@@ -12,13 +15,29 @@ def main():
         if query.lower() == "exit":
             break
         
+        if query.lower() == "clear":
+            conversation_history = []
+            print("[Memort Cleared]\n")
+            continue
+        
+        conversation_history.append({
+            'role': 'user',
+            'content': query
+        })
+        
         print("\n[AGENT] Starting research...\n")
         
         result = agent.invoke({
             "query": query,
+            "conversation_history": conversation_history,
             "tasks": [],
             "tool_results": [],
             "final_report": ""
+        })
+        
+        conversation_history.append({
+            'role': 'assistant',
+            'content': result['final_report']
         })
         
         print("\n" + "="*50)
